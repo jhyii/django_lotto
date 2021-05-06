68fef140a7ca486dc0d06ed35197cd50736f6cd5
jhyii <kandy1998@naver.com>
2021-05-06 10:02:21 +0900

    First commit

---
+++ lotto/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .models import GuessNumbers
+
+# Create your tests here.
+class GuessNumbersTestCase(TestCase):
+
+    def test_generate(self):
+        g = GuessNumbers(name='Test numbers', text='selected numbers')
+
+        print('Pre-generate : ', g.lottos)
+
+        g.generate()
+
+        print(g.update_date)
+        print(g.lottos)
+
+        self.assertTrue(len(g.lottos) > 20 )
